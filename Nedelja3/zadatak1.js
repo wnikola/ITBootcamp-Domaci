55baee268fd8c8c454f1b63cdb28d1dd5edb78c3
Nikola M <mihajlovicn44@gmail.com>
2019-11-06 20:50:18 +0100

    Prepravljen zadatak 1

--- Nedelja3/zadatak1.js
+++ Nedelja3/zadatak1.js
@@ -2,16 +2,14 @@ function maksTriBroja(a, b, c) {
   if (isNaN(a) || isNaN(b) || isNaN(c)) {
     return 'Pogresno unet argument';
   } else {
-    if (a > b && a > c) {
+    if (a >= b && a >= c) {
       return a;
-    } else if (b > a && b > c) {
+    } else if (b >= a && b >= c) {
       return b;
-    } else if (c > a && c > b) {
-      return c;
     } else {
-      return 'Brojevi su jednaki';
+      return c;
     }
   }
 }
 
-console.log(maksTriBroja(5, 2, 3));
+console.log(maksTriBroja(5, 2, 5));
