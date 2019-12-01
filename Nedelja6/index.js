abd1766adb96fbe94863f56484dac76530891ee5
Nikola M <mihajlovicn44@gmail.com>
2019-12-01 16:47:46 +0100

    Dodat ostatak domaceg za sestu nedelju

---
+++ Nedelja6/index.js
@@ -0,0 +1,28 @@
+const loginBtn = document.querySelector('#login');
+const userTxt = document.querySelector('#username');
+const passTxt = document.querySelector('#password');
+const usrErr = document.querySelector('#usr-err');
+
+loginBtn.addEventListener('click', () => {
+  if (/^[0-9]/.test(userTxt.value) === true) return usrErr.style.display = 'block';
+
+  fetch('./korisnici.json')
+    .then(res => res.json())
+    .then(data => {
+      if (data.find(el => el.username === userTxt.value) === undefined) return alert('User not found');
+      data.forEach(el => {
+        if (el.username === userTxt.value && el.password === passTxt.value) {
+          alert('Logged in');
+          userTxt.value = '';
+          passTxt.value = '';
+        } else if (el.username === userTxt.value && el.password !== passTxt.value) {
+          alert('Wrong password');
+        }
+      });
+      usrErr.style.display = 'none';
+    });
+});
+
+// JSON fajl sa korisnicima bi nam bio na backend-u (tj. verovatno bismo imali bazu podataka)
+// Na backend-u bi postojao API na koji bismo podatke iz forme poslali pomocu HTTP POST zahteva
+// Backend bi vrsio proveru podataka na osnovu koje bi nam vratio true/false
